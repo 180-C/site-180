cf3bf3ab1d0e6b7da32fee4f0faabfbc0d8608b3
Antonin Faure <antonin.faure@epfl.ch>
2023-09-08 17:02:12 +0200

    shuffle crieur slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,35 +39,33 @@
     },
   });
 
+  $.fn.shuffle = function() {
+ 
+    var allElems = this.get(),
+       getRandom = function(max) {
+         return Math.floor(Math.random() * max);
+       },
+       shuffled = $.map(allElems, function(){
+         var random = getRandom(allElems.length),
+             randEl = $(allElems[random]).clone(true)[0];
+         allElems.splice(random, 1);
+         return randEl;
+       });
+ 
+    this.each(function(i){
+       $(this).replaceWith($(shuffled[i]));
+     });
+ 
+    return $(shuffled);
+ 
+  };
+ 
   // Crieur Slider
   // ----------------------------------------
   $('.crieur-slider').each(function (index, elm) {
 
-    function shuffle () {
- 
-      var allElems = this.get(),
-         getRandom = function(max) {
-           return Math.floor(Math.random() * max);
-         },
-         shuffled = $.map(allElems, function(){
-           var random = getRandom(allElems.length),
-               randEl = $(allElems[random]).clone(true)[0];
-           allElems.splice(random, 1);
-           return randEl;
-         });
-   
-      this.each(function(i){
-         $(this).replaceWith($(shuffled[i]));
-       });
-   
-      return $(shuffled);
-   
-    };
-
     var slides = $(elm).find('.swiper-slide');
     slides.shuffle();
-
-    console.log(slides)
 
     new Swiper(".crieur-slider", {
       spaceBetween: 24,
